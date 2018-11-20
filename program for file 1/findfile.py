fc788f7f2f18d0c0f99e7e91c041acfcc55a2c0d
upadrajat <37922575+upadrajat@users.github.com>
2018-11-20 21:37:21 +0530

    test

---
+++ program for file 1/findfile.py
@@ -0,0 +1,29 @@
+import os
+def getListOfFiles(dirName):
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    for entry in listOfFile:
+        fullPath = os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles        
+ 
+def main():
+    dirName=input("Input directory " )
+   
+    listOfFiles = getListOfFiles(dirName)
+    
+   
+    for elem in listOfFiles:
+        print(elem)
+    listOfFiles = list()
+    for (dirpath, dirnames, filenames) in os.walk(dirName):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+    for elem in listOfFiles:
+        print(elem)    
+        
+if __name__ == '__main__':
+    main()
